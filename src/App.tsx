694f1d4f1f4ebe7661156520e6c400412f07c481
Jailson Paiva <j.psherl1009@gmail.com>
2021-12-19 16:26:57 -0400

    Header add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components'
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
 
-const Title = styled.h1 `
-  font-size: 64px;
-  color: #8257e6;
-`
-
-export function App(): JSX.Element {
+export function App(){
   return (
     <div className="App">
-      <Title>Hellor world</Title>
+      <Header />
+      <GlobalStyle />
     </div>
   );
 }
