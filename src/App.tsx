1a2ba23ce0022d9d0b8be49a75c00174f7519e76
Jailson Paiva <j.psherl1009@gmail.com>
2022-02-22 11:10:55 -0400

    Modal and modal form create

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import { NewTrasactionModal } from './components/NewTrasactionModal';
 
 export function App(){
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header onOperNewTrasactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTrasactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,26 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import logoImg from '../../assets/logo.svg';
 
-export function Header() {
+import { Container, Content } from './styles';
+import { useState } from 'react';
+
+interface HanderProps {
+    onOperNewTrasactionModal: () => void;
+}
+
+export function Header({ onOperNewTrasactionModal }: HanderProps) {
+    
+
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
 
-                <button type='button'>Nova transação</button>
+                <button type='button' onClick={onOperNewTrasactionModal}>
+                    Nova transação
+                </button>
+
+               
             </Content>
         </Container>
     )
